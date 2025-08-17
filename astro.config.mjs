937bec75fcf1bc2325641c8547bf855fdc87c99e
mandar-gite <mandar.gite@gmail.com>
2025-08-18 01:47:52 +0530

    cleaned .mjs file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,3 @@
-```
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-// The brief specifies a static export for GitHub Pages, which is handled
-// by setting the output to 'static'.
-export default defineConfig({
-  site: 'https://72ai.in',
-  base: '/',
-  output: 'static',
-  integrations: [tailwind()],
-});
-
-```
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
