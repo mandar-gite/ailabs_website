e9290df429b270479079a5846352fdca76bf3cff
Mandar Gite <mandar.gite@gmail.com>
2025-08-31 18:51:34 +0530

    Add Tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://mandar-gite.github.io',
   base: '/ailabs_website',
+  integrations: [tailwind()],
   build: {
     assets: 'assets'
   }
