bd41a523aa3c9edf7163e5750dadef8e321d6761
Mandar Gite <mandar.gite@gmail.com>
2025-08-31 18:52:44 +0530

    Conditionally set base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   site: 'https://mandar-gite.github.io',
-  base: '/ailabs_website',
+  ...(isProduction && { base: '/ailabs_website' }),
   build: {
     assets: 'assets'
   }
